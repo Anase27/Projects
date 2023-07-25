847439e49fb8142baacbe5a939b87118e3cfef4b
Anase72 <ankit2184.be21@chitkara.edu.in>
2023-07-25 20:59:33 +0530

    styling done only js need work

---
+++ sign-up form/sign-up-form.js
@@ -0,0 +1,11 @@
+let pass = document.querySelector("#pass");
+let confirmPass = document.querySelector("#confirmPass");
+let f = document.querySelector("#f_name");
+
+f.addEventListener('input',() =>{
+    let k = document.querySelector("#f_name");
+    console.log(k.value);
+    // if (pass.value!=confirmPass.value) {
+        
+    // }
+});
