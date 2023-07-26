905a41dbeb6458ebc702fb75a65a021dbad75a74
Anase72 <ankit2184.be21@chitkara.edu.in>
2023-07-26 12:38:29 +0530

    sign-up page completed

--- sign-up form/sign-up-form.js
+++ sign-up form/sign-up-form.js
@@ -1,11 +1,22 @@
 let pass = document.querySelector("#pass");
 let confirmPass = document.querySelector("#confirmPass");
-let f = document.querySelector("#f_name");
 
-f.addEventListener('input',() =>{
-    let k = document.querySelector("#f_name");
-    console.log(k.value);
-    // if (pass.value!=confirmPass.value) {
-        
-    // }
-});
+
+confirmPass.addEventListener('input',matchPassword);
+pass.addEventListener('input',matchPassword);
+
+function matchPassword(){
+    let originalPass = document.querySelector("#pass");
+    let confirmationPass = document.querySelector("#confirmPass");
+
+    if (confirmationPass.value != originalPass.value){
+        if (!confirmationPass.hasAttribute("style")) {
+            confirmPass.setCustomValidity("Password do not match");
+            confirmPass.setAttribute("style","outline: 2px solid #ea1f18");
+        }
+    }
+    else if (confirmationPass.value == originalPass.value) {
+        confirmPass.setCustomValidity("");
+        confirmPass.removeAttribute("style");
+    }
+}
