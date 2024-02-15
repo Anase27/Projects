ef1bcdddd8b5c41098a5750eb41129276b7d86f9
Anase72 <ankit2184.be21@chitkara.edu.in>
2024-02-16 00:01:22 +0530

    made some minor changes of indentation

--- to-do-list/webpack.common.js
+++ to-do-list/webpack.common.js
@@ -1,17 +1,17 @@
 const path = require('path');
 
 module.exports = {
-    entry: "./src/index.js",
+	entry: "./src/index.js",
 
-    module: {
-        rules: [
-            {
-                test: /\.(png|svg|jpg|avif|jpeg)$/i,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'Images/[name].[fullhash][ext]'
-                }
-            },
-        ]
-    }
+	module: {
+		rules: [
+			{
+				test: /\.(png|svg|jpg|avif|jpeg)$/i,
+				type: 'asset/resource',
+				generator: {
+					filename: 'Images/[name].[fullhash][ext]'
+				}
+			},
+		]
+	}
 }
--- to-do-list/webpack.prod.js
+++ to-do-list/webpack.prod.js
@@ -7,40 +7,40 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, { 
-    mode: 'production',
-    devtool: 'source-map',
-    output: {
-        filename: '[name].[fullhash].js',
-        path: path.resolve(__dirname,'dist'),
-        clean: true,
-    },
+	mode: 'production',
+	devtool: 'source-map',
+	output: {
+		filename: '[name].[fullhash].js',
+		path: path.resolve(__dirname,'dist'),
+		clean: true,
+	},
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[fullhash].css'
-        }),
-        new CleanWebpackPlugin(),
-    ],
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: '[name].[fullhash].css'
+		}),
+		new CleanWebpackPlugin(),
+	],
 
-    module:{
-        rules:[
-            {
-                test: /\.css$/i,
-                use:[
-                    MiniCssExtractPlugin.loader,//2 extract css into file
-                    'css-loader' //1. Turns Css into common.js or processes css to make it usable within js file
-                ],
-            },
-        ],
-    },
+	module:{
+		rules:[
+			{
+				test: /\.css$/i,
+				use:[
+					MiniCssExtractPlugin.loader,//2 extract css into file
+					'css-loader' //1. Turns Css into common.js or processes css to make it usable within js file
+				],
+			},
+		],
+	},
 
-    optimization: {
-        minimizer:[
-            new CssMinimizerPlugin(),
-        ]
-    }
+	optimization: {
+		minimizer:[
+			new CssMinimizerPlugin(),
+		]
+	}
     
 });
