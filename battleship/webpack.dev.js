101f00935f592dfe74f6ba00dbe6d41492567da7
Anase72 <ankit2184.be21@chitkara.edu.in>
2024-02-16 00:03:31 +0530

    created webpack confing files and added some dependencies

---
+++ battleship/webpack.dev.js
@@ -0,0 +1,22 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    
+    module:{
+		rules:[
+			{
+				test: /\.css$/i,
+				use:[
+					'style-loader', //2. inject styles into DOM
+					'css-loader' //1. Turns Css into common.js or processes css to make it usable within js file
+				]
+			},
+		],
+	},
+})
